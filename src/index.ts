a6d8910ba57dc1e335cb9a4c5a42f47689e8ddf0
kjirou <kjirou.web@gmail.com>
2022-12-11 12:01:23 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
-export type Foo = {
-  x: number;
+type DevelopmentKind = "design" | "management" | "programming";
+
+type SkillKind = "design-architecture" | "design-markup" | "design-ui";
+
+type Development = {
+  value: number;
 };
-const foo: Foo = { x: 1 };
-console.log(foo);
+
+type Project = {
+  // TODO: 作業内容のモデリング
+  //       いろいろな作業の種類があり、それぞれに得意な職種がいるって方が良さそう
+  necessaryDevelopments: { [k in DevelopmentKind]: number };
+};
+
+type Game = {
+  /** 現在のターン番号。1 開始の連番。フレーバー上は「週」になる。 */
+  currentTurnNumber: number;
+  /** 0 以上 1 未満の乱数を生成する関数。 */
+  getRandom: () => number;
+};
+
+const proceedToNextTurn = (game: Game): Game => {
+  const nextTurnNumber = game.currentTurnNumber + 1;
+  return { ...game, currentTurnNumber: nextTurnNumber };
+};
+
+let game: Game = {
+  currentTurnNumber: 1,
+  getRandom: () => Math.random(),
+};
+game = proceedToNextTurn(game);
+game = proceedToNextTurn(game);
+console.log(game);
